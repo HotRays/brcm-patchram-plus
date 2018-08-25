efee08e307ce99ec68b7b775347184613c2c5e4b
fengmushu <fengmushu@gmail.com>
2018-08-25 17:48:32 +0800

    fixup bdaddr

--- btstart.sh
+++ btstart.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+#--bd_addr 43:42:3b:86:1d:08 \
+
 /usr/bin/brcm_patchram_plus \
     --patchram /lib/firmware/brcm/bcm4345c0.hcd \
     --baudrate 3000000 \
     --enable_lpm \
     --enable_hci \
-    --no2bytes /dev/ttyS1 \
-    --bd_addr 43:42:3b:86:1d:08
+    --no2bytes \
+    /dev/ttyS1
